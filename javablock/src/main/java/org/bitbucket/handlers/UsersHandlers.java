bf7ef3ac3b9d5af2837a80bb5da6619527a6cc65
Boyko Nikolay <enzeox@gmail.com>
2021-04-13 21:59:51 +0300

    правки

--- javablock/src/main/java/org/bitbucket/handlers/UsersHandlers.java
+++ javablock/src/main/java/org/bitbucket/handlers/UsersHandlers.java
@@ -12,6 +12,7 @@ import org.bitbucket.payload.Token;
 import org.bitbucket.utils.JsonHelper;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class UsersHandlers extends HttpServlet {
 
@@ -31,5 +32,29 @@ public class UsersHandlers extends HttpServlet {
         out.write(str.getBytes());
         out.flush();
         out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        try {
+            if (!"aplication/json".equalsIgnoreCase(req.getHeader("Content-type"))) {
+                res.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid content type");
+            } else {
+                ServletOutputStream out = res.getOutputStream();
+                String body = req.getReader().lines().collect(Collectors.joining());
+                System.out.println("Body" + body);
+                UserAuthorizationDto payload = JsonHelper.fromFormat(body, UserAuthorizationDto.class)
+                        .orElseThrow(BadRequest::new);
+                Token token = this.usersController.auth(payload);
+                String str = JsonHelper.toFormat(token).orElseThrow(BadRequest::new);
+                res.setContentType("application/json");
+                res.setStatus(HttpServletResponse.SC_ACCEPTED);
+                out.write(str.getBytes());
+                out.flush();
+                out.close();
+            }
+        } catch (BadRequest e) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid body");
+        }
     }
 }
