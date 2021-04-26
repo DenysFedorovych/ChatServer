3f9449de5b5e75783c180ae70cb42af6f2e10c9f
Denys <63046982+DenysFedorovych@users.noreply.github.com>
2021-04-27 00:52:08 +0300

    Hikari is fixed and works with Heroku

--- javablock/src/main/java/org/bitbucket/config/DatabaseConfig.java
+++ javablock/src/main/java/org/bitbucket/config/DatabaseConfig.java
@@ -7,18 +7,12 @@ import org.bitbucket.repository.UsersRepository;
 import org.bitbucket.service.CustomUsersService;
 
 import javax.sql.DataSource;
+import java.io.File;
 
 public class DatabaseConfig {
 
     public static DataSource getHikariDS(){
-        HikariConfig config = new HikariConfig();
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setJdbcUrl("jdbc:postgresql://ec2-34-252-251-16.eu-west-1.compute.amazonaws.com/de28o4hdufj7m1");
-        config.setUsername("qgvrweumbrinxe");
-        config.setPassword("89c37abe70b0c0a5b337f1a55edccb6e7064a1c93eaac14db2a4d747c42e2be5");
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        HikariConfig config = new HikariConfig("src" + File.separator + "main" + File.separator + "resources" + File.separator + "hikari.properties");
         return new HikariDataSource(config);
     }
 
--- javablock/src/test/java/org/bitbucket/repository/UsersRepositoryTest.java
+++ javablock/src/test/java/org/bitbucket/repository/UsersRepositoryTest.java
@@ -1,52 +1,41 @@
 package org.bitbucket.repository;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import org.bitbucket.config.DatabaseConfig;
 import org.bitbucket.dto.UserAuthorizationDto;
 import org.bitbucket.dto.UserRegistrationDto;
 import org.bitbucket.entity.User;
-import org.bitbucket.micro.orm.CustomJdbcTemplate;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class UsersRepositoryTest {
 
-    private HikariConfig hikariConfig = new HikariConfig("src/main/resources/hikari.properties");
-    private HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
-    private UsersRepository repo = new UsersRepository(new CustomJdbcTemplate(hikariDataSource));
+    private UsersRepository repo = DatabaseConfig.getUsersRepository();
 
     private UserRegistrationDto userRegistrationDto = new UserRegistrationDto("Denys", "Fedorovych", "login", "password", "password","email", "+22222222");
-    private UserAuthorizationDto userAuthorizationDto = new UserAuthorizationDto("ffff","ffff");
-    private UserAuthorizationDto userAuthorizationDto1 = new UserAuthorizationDto("login","password");
-    private User user = new User(1,"Denys","Fedorovych","ffff","ffff","ffff","ffff");
-    private User user1 = new User(1, "Denys", "Fedorovych", "login", "password","email", "+22222222");
+    private UserAuthorizationDto userAuthorizationDto = new UserAuthorizationDto("login","password");
+    private User user1 = new User(1,"Denys","Fedorovych","ffff","login","password","ffff");
+    private User user = new User(1, "Denys", "Fedorovych", "email", "login", "password", "+22222222");
+    private UserRegistrationDto userRegistrationDto1 = new UserRegistrationDto(user1);
 
     @Test
     public void findByAuthDto() {
-        Assert.assertEquals(user, repo.findByAuthDto(userAuthorizationDto));
-    }
-
-    @Test
-    public void findById() {
-        Assert.assertEquals(user, repo.findById(1));
-    }
-
-    @Test
-    public void insert() {
         repo.insert(userRegistrationDto);
-        Assert.assertEquals(user1, repo.findByAuthDto(userAuthorizationDto1));
+        Assert.assertEquals(new UserRegistrationDto(user), new UserRegistrationDto(repo.findByAuthDto(userAuthorizationDto)));
+        repo.delete(userRegistrationDto);
     }
 
     @Test
-    public void delete() {
+    public void insertAndDelete() {
+        repo.insert(userRegistrationDto);
         repo.delete(userRegistrationDto);
-        Assert.assertEquals(null, repo.findByAuthDto(userAuthorizationDto1));
+        Assert.assertEquals(null, repo.findByAuthDto(userAuthorizationDto));
     }
 
     @Test
     public void update() {
-        repo.update(new UserRegistrationDto("Denys", "Fedorovych", "login", "password", "password","email", "+3333333"));
+        repo.insert(userRegistrationDto);
+        repo.update(userRegistrationDto1);
+        Assert.assertEquals(userRegistrationDto1, new UserRegistrationDto(repo.findByAuthDto(new UserAuthorizationDto(userRegistrationDto1))));
+        repo.delete(userRegistrationDto1);
     }
 }
