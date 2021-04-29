7948aa5f2cece0589d80b488d7196eccd7b823aa
Denys <63046982+DenysFedorovych@users.noreply.github.com>
2021-04-29 17:28:04 +0300

    Jar file is fixed, WsContextListener added to context

--- javablock/src/main/java/org/bitbucket/config/AppConfig.java
+++ javablock/src/main/java/org/bitbucket/config/AppConfig.java
@@ -1,4 +1,22 @@
 package org.bitbucket.config;
 
+import org.apache.catalina.LifecycleException;
+import org.bitbucket.handlers.WebSocketHandler;
+import org.bitbucket.utils.WrapTomcat;
+
+import javax.servlet.ServletException;
+
 public class AppConfig {
+
+    public static void startTomcat() {
+        try {
+            WrapTomcat t = ServerConfig.tomcat();
+            t.start();
+            t.websocketRegistry(new WebSocketHandler());
+            t.getServer().await();
+        } catch (ServletException|LifecycleException e) {
+            System.out.printf("Enter:%s",e.getMessage());
+        }
+    }
+
 }
--- javablock/src/main/java/org/bitbucket/config/ServerConfig.java
+++ javablock/src/main/java/org/bitbucket/config/ServerConfig.java
@@ -1,50 +1,26 @@
 package org.bitbucket.config;
 
 import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-import org.bitbucket.handlers.WebSocketHandler;
+import org.apache.tomcat.websocket.server.WsContextListener;
+import org.bitbucket.utils.WrapTomcat;
 
 import javax.servlet.ServletException;
-import javax.websocket.DeploymentException;
-import javax.websocket.server.ServerContainer;
-import javax.websocket.server.ServerEndpointConfig;
 import java.io.File;
 
 public class ServerConfig {
-    public static void tomcat() throws ServletException, LifecycleException {
-        Tomcat tomcat = new Tomcat();
+    public static WrapTomcat tomcat() throws ServletException {
+        WrapTomcat tomcat = new WrapTomcat();
 
         String webPort = System.getenv("PORT");
         if(webPort == null || webPort.isEmpty()) {
             webPort = "5432";
         }
-
         tomcat.setPort(Integer.parseInt(webPort));
         Context ctx = tomcat.addWebapp("/", new File(".").getAbsolutePath());
+        ctx.addApplicationListener(WsContextListener.class.getName());
+        tomcat.setCtx(ctx);
         tomcat.addServlet("","UsersHandler",HandlerConfig.usersHandlers());
         ctx.addServletMappingDecoded("/","UsersHandler");
-
-        tomcat.start();
-        ServerContainer scon = (ServerContainer) ctx.getServletContext().getAttribute(ServerContainer.class.getName());
-        try {
-            scon.addEndpoint(ServerEndpointConfig.Builder.create(WebSocketHandler.class,"/echo")
-                    .configurator(new ServerEndpointConfig.Configurator() {
-                @Override
-                public <T> T getEndpointInstance(Class<T> clazz) throws InstantiationException {
-                    return (T) new WebSocketHandler();
-                }
-            }).build());
-        } catch (DeploymentException e) {
-            e.printStackTrace();
-        }
-        tomcat.getServer().await();
+        return tomcat;
     }
-
-    public static ServerEndpointConfig.Configurator sec = new ServerEndpointConfig.Configurator() {
-        @Override
-        public <T> T getEndpointInstance(Class<T> clazz) throws InstantiationException {
-            return (T) new WebSocketHandler();
-        }
-    };
 }
