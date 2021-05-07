615162982c446bcc08781f4ee7dd896f7b79196b
Denys <63046982+DenysFedorovych@users.noreply.github.com>
2021-05-07 15:40:25 +0300

    New branches created

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,4 @@
 function login() {
-    // let xhr = new XMLHttpRequest();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -8,35 +7,6 @@ function login() {
         login: username,
         password: password
     };
-
-    //
-    // xhr.open('POST', 'http://localhost:8080/login/auth', true);
-    // xhr.setRequestHeader('Content-Type', 'application/json');
-    // xhr.onreadystatechange = function () {
-    //     if (xhr.readyState === 4 && xhr.status === 200) {
-    //         result.innerHTML = this.responseText;
-    //     }
-    // };
-    // xhr.send(JSON.stringify(logIn));
-    // xhr.onload = function () {
-    //     if(xhr.status >= 200 && xhr.status < 300) {
-    //         // document.cookie = xhr.getResponseHeader('Set-Cookie');
-    //         const hui = xhr.getResponseHeader('Set-Cookie');
-    //         console.log(xhr.response);
-    //         let buff = '';
-    //         for (let i = 0; i < hui.length; i++) {
-    //             if (hui.charAt(i) !== ';') {
-    //                 buff += hui.charAt(i)
-    //             }
-    //         }
-    //         console.log(buff);
-    //         console.log(document.cookie);
-    //         console.log('registration good');
-    //         //document.location = "..\\html\\chatwindow.html";
-    //     } else {
-    //         console.log(xhr.statusText);
-    //     }
-    // }
 
     axios.post("http://localhost:8080/login/auth", logIn)
         .then((response) => {
@@ -88,19 +58,5 @@ async function registration() {
             console.log(xhr.statusText);
         }
     }
-
-    // axios.post('http://localhost:8080/login/registration', regIn)
-    //     .then((response) => {
-    //         console.log(response.data);
-    //         console.log('registration good');
-    //         if (response.status >= 200 && response.status < 300) {
-    //             console.log('registration good');
-    //             setTimeout(50000);
-    //             document.location = '..\\html\\main.html';
-    //         }
-    //     }, (error) => {
-    //         console.log(error);
-    //     });
-
 
 }
